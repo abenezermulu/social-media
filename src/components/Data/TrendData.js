1c05e49f0105dd93bf4aa9cc27fc7c87db9b7aa5
AbenezerM <abenezermulugeta41@gmail.com>
2022-09-24 01:32:43 -0500

    Right sidebar - Trending section and share button

---
+++ src/components/Data/TrendData.js
@@ -0,0 +1,22 @@
+export const TrendData = [
+    {
+        name: "Minions", 
+        shares: 97
+    },
+    {
+        name: "Avengers", 
+        shares: 80.5
+    },
+    {
+        name: "Shale", 
+        shares: 73
+    },
+    {
+        name: "React JS", 
+        shares: 73
+    },
+    {
+        name: "Elon Musk", 
+        shares: 68
+    },
+]
