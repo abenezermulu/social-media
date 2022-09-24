010e931a6949577b5d80a440c536b53b50bbbe5a
AbenezerM <abenezermulugeta41@gmail.com>
2022-09-23 22:24:54 -0500

    Post sharing front end functionality

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import PostSide from "../../components/PostSide/PostSide"
 import ProfileSide from "../../components/profileSide/ProfileSide"
 import './Home.css'
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div className="Home">
             <ProfileSide />
+            <PostSide />
             <div className="postSide">Post</div>
             <div className="rightSide">Rigth</div>
         </div>
